c3110a392e22921704d0f7f03b9415599343a281
Cyrus Vandrevala <cyrus.vandrevala@gmail.com>
2013-06-14 10:42:52 -0400

    Rename to html

--- footer.php
+++ footer.php
@@ -1,4 +1,1 @@
-<div id="footer">
-	<p><span>&copy; 2012 <a href="#">Cyrus Vandrevala</a></span><br />
-	Template design by <a href="http://andreasviklund.com/">Andreas Viklund</a></p>
-</div>
+
--- header.php
+++ header.php
@@ -1,8 +1,1 @@
-<head>
-	<meta name="description" content="My Personal Website Hosted by Purdue University">
-	<meta name="keywords" content="physics, graduate, student, quantum, computing, Purdue">
-	<meta name="keywords" content="university, electricity, optics, PHYS">
-	<meta name="author" content="Cyrus Vandrevala">
-	<link rel="stylesheet" type="text/css" href="variant-whitespace.css" media="all">
-	<title>Cyrus' Purdue Webpage</title>
-</head>
+
